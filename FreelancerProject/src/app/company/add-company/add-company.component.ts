3aef0c23261177ddd027f6bb3b50784ff53186cb
AliKiyak <r0702374@student.thomasmore.be>
2019-12-09 13:04:06 +0100

    tags

--- FreelancerProject/src/app/company/add-company/add-company.component.ts
+++ FreelancerProject/src/app/company/add-company/add-company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/Models/company.model';
+import { ContactInfo } from 'src/app/Models/contact-info.model';
+import { Tag } from 'src/app/Models/tag.model';
 
 @Component({
   selector: 'app-add-company',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCompanyComponent implements OnInit {
 
+  company: any = {};
+  location: any = {};
+  contactInfo: any = {};
+  tags: string[] = [];
+  tag = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  addTag(event) {
+    this.tags.push(this.tag);
+    this.tag = '';
+    event.preventDefault();
+  }
+
+  onSubmit() {
+    const addTags: Tag[] = [];
+    this.tags.forEach(element => {
+      addTags.push(new Tag(0, element));
+    });
+    const newCompany = new Company(0, null, null, null, addTags, this.location,
+      this.company.CompanyName, this.contactInfo, this.company.About);
+
+    console.log(newCompany);
+
+  }
 }
