ee2b94512af5e213435742a8f76b995725576942
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-10 17:04:38 +0100

    1.0.0

--- FreelancerProject/src/app/company/edit-company/edit-company.component.ts
+++ FreelancerProject/src/app/company/edit-company/edit-company.component.ts
@@ -13,15 +13,17 @@ import { Company } from 'src/app/Models/company.model';
 export class EditCompanyComponent implements OnInit {
 
   id = '';
-  company: any = {};
+  company: Company;
   location: any = {};
   contactInfo: any = {};
-  tags: string[] = [];
   tag = '';
+  tagToAdd:Tag;
+  tagCompany:TagCompany;
+
   constructor(private readonly companyService: CompanyService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.companyService.currentCompany.subscribe( (res: any) => {
+    this.companyService.currentCompany.subscribe((res: any) => {
       this.id = res;
       this.getCompany(this.id);
     });
@@ -30,34 +32,28 @@ export class EditCompanyComponent implements OnInit {
 
   getCompany(id) {
     this.companyService.getCompanyDetail(id).subscribe(
-      result => { console.log(result); this.company = result;
-                  this.fillTags(result.tagCompanies); 
-                }
+      result => {
+        this.company = result;
+      }
     );
   }
 
-  fillTags(tags) {
-    console.log(tags);
-    tags.forEach(element => {
-      this.tags.push(element.tag.tagName);
-    });
-    console.log(this.tags);
-  }
 
   addTag(event) {
-    this.tags.push(this.tag);
+    this.tagToAdd = new Tag(0, this.tag)
+    this.tagCompany = new TagCompany(0, null, this.tagToAdd);
+    this.company.tagCompanies.push(this.tagCompany);
     this.tag = '';
     event.preventDefault();
   }
 
   onSubmit() {
-    const addTags: TagCompany[] = [];
-    this.tags.forEach(element => {
-      this.company.tagCompanies.push(new TagCompany(0, null, new Tag( 0, element)));
-    });
-    console.log(this.company);
     this.companyService.updateCompany(this.company).subscribe(
-      result => { console.log(result); this.router.navigate(['']);  }
+      result => {
+        console.log(result);
+        this.companyService.currentCompany.next(this.company.companyID);
+        this.ngOnInit();
+      }
     );
   }
 }
