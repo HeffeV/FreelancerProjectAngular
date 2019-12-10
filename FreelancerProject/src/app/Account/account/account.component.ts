0868bb936b6b505ec94cd4b890c06bbfd2f40252
Kjelle Gyesbreghs <gyesbreghskjelle@hotmail.com>
2019-12-10 11:15:38 +0100

    merge

--- FreelancerProject/src/app/Account/account/account.component.ts
+++ FreelancerProject/src/app/Account/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/Services/account.service';
+import { User } from 'src/app/Models/user.model';
 
 @Component({
   selector: 'app-account',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  user : User = null;
+
+  constructor(private _accountService: AccountService) { 
+    this.loadUser();
+  }
+
+  loadUser(){
+    //this._accountService.getUser()
+  }
 
   ngOnInit() {
   }
