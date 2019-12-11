c37936625c3806ce81f41affe57f42402cee48df
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-11 19:42:45 +0100

    Authguard

--- FreelancerProject/src/app/Models/user-assignment.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserAssignment } from './user-assignment.model';
-
-describe('UserAssignment', () => {
-  it('should create an instance', () => {
-    expect(new UserAssignment()).toBeTruthy();
-  });
-});
