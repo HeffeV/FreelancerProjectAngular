2636943ebc2f457a8275272f237e415aa9a84d5d
Jeff <38490982+HeffeV@users.noreply.github.com>
2019-12-13 17:15:16 +0100

    Admin dashboard

--- FreelancerProject/src/app/Dashboard/dashboard/dashboards/dashboard-admin/manageskills/manageskills.component.ts
+++ FreelancerProject/src/app/Dashboard/dashboard/dashboards/dashboard-admin/manageskills/manageskills.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { SkillService } from 'src/app/Services/skill.service';
+import { Category } from 'src/app/Models/category.model';
+import { Skill } from 'src/app/Models/skill.model';
 
 @Component({
   selector: 'app-manageskills',
@@ -9,21 +11,113 @@ import { SkillService } from 'src/app/Services/skill.service';
 })
 export class ManageskillsComponent implements OnInit {
 
+  success: boolean;
+  successInfo: string = "";
+  registerError: string = "";
+  error: boolean;
+  categories: Category[];
+  category: Category;
+  skill: Skill;
+  skills: Skill[];
 
-  createForm = this.fb.group({
-    TagName: [''],
+  createCategoryForm = this.fb.group({
+    CategoryName: [''],
   })
-  editForm = this.fb.group({
-    TagName: [''],
-    Tag: [''],
+  createSkillForm = this.fb.group({
+    Category: [''],
+    Skill: [''],
   })
-  deleteForm = this.fb.group({
-    Tag: [''],
+  deleteSkillForm = this.fb.group({
+    Skill: [''],
+  })
+  deleteCategoryForm = this.fb.group({
+    Category: [''],
   })
 
   constructor(private fb: FormBuilder, private skillservice: SkillService) { }
 
   ngOnInit() {
+    this.skillservice.getAllCategories().subscribe(e => {
+      this.categories = e;
+    });
+    this.skillservice.getAllSkills().subscribe(e => {
+      this.skills = e;
+    })
+    this.createCategoryForm.reset();
+    this.createSkillForm.reset();
+    this.deleteSkillForm.reset();
+    this.deleteCategoryForm.reset();
+  }
+
+  createCategory() {
+    this.category = new Category(0, this.createCategoryForm.value.CategoryName);
+    this.skillservice.createCategory(this.category).subscribe((res: any) => {
+      this.success = true;
+      this.successInfo = this.category.categoryName + " created successfully.";
+      this.error = false;
+      this.ngOnInit();
+    },
+      e => {
+        if (e.status == 404) {
+          this.registerError = this.category.categoryName + ' already exists! ';
+        }
+        else {
+          this.registerError = 'Something went wrong, please try again later.';
+        }
+        this.success = false;
+        this.error = true;
+      });
+  }
+
+  createSkill() {
+    this.skill = new Skill(0,
+      this.createSkillForm.value.Skill,
+      new Category(0, this.createSkillForm.value.Category));
+
+    this.skillservice.createSkill(this.skill).subscribe((res: any) => {
+      this.success = true;
+      this.error = false;
+      this.successInfo = this.skill.skillName + " created successfully.";
+      this.ngOnInit();
+    },
+      e => {
+        if (e.status == 404) {
+          this.registerError = this.skill.skillName + ' already exists! ';
+        }
+        else {
+          this.registerError = 'Something went wrong, please try again later.';
+        }
+        this.success = false;
+        this.error = true;
+      })
+  }
+
+  deleteSkill() {
+    this.skillservice.deleteSkill(this.deleteSkillForm.value.Skill).subscribe((res: any) => {
+      this.success = true;
+      this.error = false;
+      this.successInfo = "Deleted skill successfully.";
+      this.ngOnInit();
+    },
+      e => {
+        this.registerError = 'Something went wrong, please try again later.';
+        this.success = false;
+        this.error = true;
+      })
+  }
+
+  deleteCategory() {
+    this.skillservice.deleteCategory(this.deleteCategoryForm.value.Category).subscribe((res: any) => {
+      this.success = true;
+      this.error = false;
+      this.successInfo = "Deleted category successfully.";
+      this.ngOnInit();
+    },
+      e => {
+        this.registerError = 'Something went wrong, please try again later.';
+        this.success = false;
+        this.error = true;
+      })
   }
 
 }
--- FreelancerProject/src/app/Services/skill.service.ts
+++ FreelancerProject/src/app/Services/skill.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../Models/category.model';
+import { Skill } from '../Models/skill.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,25 @@ export class SkillService {
   constructor(private http: HttpClient) { }
 
   getAllSkills() {
-
+    return this.http.get<Skill[]>("https://freelancerprojectapi.azurewebsites.net/api/Skill/getSkills");
   }
 
   getAllCategories() {
-
+    return this.http.get<Category[]>("https://freelancerprojectapi.azurewebsites.net/api/Skill/getCategories");
   }
 
+  createCategory(category: Category) {
+    return this.http.post<Category>("https://freelancerprojectapi.azurewebsites.net/api/Skill/PostCategory", category);
+  }
+  createSkill(skill: Skill) {
+    return this.http.post<Skill>("https://freelancerprojectapi.azurewebsites.net/api/Skill/PostSkill", skill);
+  }
+
+  deleteCategory(id: number) {
+    return this.http.delete<Category>("https://freelancerprojectapi.azurewebsites.net/api/Skill/DeleteCategory?id=" + id);
+  }
+
+  deleteSkill(id: number) {
+    return this.http.delete<Skill>("https://freelancerprojectapi.azurewebsites.net/api/Skill/DeleteSkill?id=" + id);
+  }
 }
